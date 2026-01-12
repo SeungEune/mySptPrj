8f9d01395482fc4c1564eaef41b0da580bff11d5
이승은 <dltmddms4860@gmail.com>
2026-01-12 10:38:15 +0900

    화면 퍼블리싱 편의성을 위한 템플릿 화면 조회 기능  커밋

---
+++ src/main/java/biz/pub/web/PublishController.java
@@ -0,0 +1,33 @@
+package biz.pub.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 퍼블리싱에 사용하기 위한 컨트롤
+ */
+@Controller
+@RequestMapping(value = "/pub")
+public class PublishController {
+
+    @GetMapping(value = "/callPopup.do")
+    public String callPopup() {
+        return "pub/callPopup";
+    }
+
+    @GetMapping(value = "/template1.do")
+    public String template1() {
+        return "pub/temp/template1";
+    }
+
+    /*
+     * 팝업 영역
+     * 1. [기능명]  /pub/popup/template1.do?index=1
+     * */
+    @GetMapping(value = "/popup/template.do")
+    public String popupTemplate1(@RequestParam(name = "index") int index) {
+        return "pub/popup/template" + index;
+    }
+}
